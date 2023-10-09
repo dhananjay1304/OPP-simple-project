ce9204c437cdb9f6144b9fd4b180694e0cf66fa0
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-10-09 13:49:56 +0530

    productList

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const products = [
-  {
-    title: "A pillo",
-    imageUrl:
-      "https://th.bing.com/th/id/OIP.0GnNiIvrxcjFpGcejBgYBQAAAA?pid=ImgDet&rs=1",
-    price: 19.99,
-    description: "A soft pillow!",
-  },
-
-  {
-    title: "iPhone",
-    imageUrl:
-      "https://th.bing.com/th/id/OIP.pI_mcb2vSdHcQzHBretQLAHaHa?w=169&h=180&c=7&r=0&o=5&pid=1.7",
-    price: 99.98,
-    description: "expensive iphone with so much quality.",
-  },
-];
-
+const productList = {
+  products: [
+    {
+      title: "A pillo",
+      imageUrl:
+        "https://th.bing.com/th/id/OIP.0GnNiIvrxcjFpGcejBgYBQAAAA?pid=ImgDet&rs=1",
+      price: 19.99,
+      description: "A soft pillow!",
+    },
+  
+    {
+      title: "iPhone",
+      imageUrl:
+        "https://th.bing.com/th/id/OIP.pI_mcb2vSdHcQzHBretQLAHaHa?w=169&h=180&c=7&r=0&o=5&pid=1.7",
+      price: 99.98,
+      description: "expensive iphone with so much quality.",
+    },
+  ],
+  
+};
